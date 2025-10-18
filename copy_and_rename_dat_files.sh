d092814e7e2b756dcb2f22f22f010bc6f7a1e3c9
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-18 20:51:33 +0900

    update copy_and_rename_dat_files, create readme

--- copy_and_rename_dat_files.sh
+++ copy_and_rename_dat_files.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 # copy_and_rename_dat_files.sh
 
-###
-### STEP 1: Get list of figure number in the paper.
-###
-#source ./list_figure_numbers.txt
-fig_number_scalar_order_parameter_passive_homeotropic="3a"
-###
-### STEP 2: Copy and rename dat files.
-###
-path_to_datafile_scalar_order_parameter_passive_homeotropic="/Volumes/matsukiyo.hiroki/Documents/simulation11_Angheluta_with_viscosity_and_with_pillar/RESULTS_REV/N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis/scalar_order_parameter_S_N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis_step_0000200000.dat"
-if [ ! -e ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ]; then
-	echo -e "\033[0;31m
-	Can't find datafile.
-	filename:
-	${path_to_datafile_scalar_order_parameter_passive_homeotropic}
-	\033[0;39m"
-	exit 1
-fi
-mkdir -p data_files
-cp ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ./data_files/
+###   ###
+###   ### STEP 1: Get list of figure number in the paper.
+###   ###
+###   #source ./list_figure_numbers.txt
+###   fig_number_scalar_order_parameter_passive_homeotropic="3a"
+###   ###
+###   ### STEP 2: Copy and rename dat files.
+###   ###
+###   path_to_datafile_scalar_order_parameter_passive_homeotropic="/Volumes/matsukiyo.hiroki/Documents/simulation11_Angheluta_with_viscosity_and_with_pillar/RESULTS_REV/N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis/scalar_order_parameter_S_N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis_step_0000200000.dat"
+###   if [ ! -e ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ]; then
+###   	echo -e "\033[0;31m
+###   	Can't find datafile.
+###   	filename:
+###   	${path_to_datafile_scalar_order_parameter_passive_homeotropic}
+###   	\033[0;39m"
+###   	exit 1
+###   fi
+###   mkdir -p data_files
+###   cp ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ./data_files/
 ###
 ### Create readme.md
 ###
+source ./list_fig_number_in_paper.txt
 : > readme.md
 echo "# Correspondence table between figure names, data files and scripts to create figures" >> readme.md
 echo "|Figure Number|Data file|Script|"                                                      >> readme.md
 echo "| ---- | ---- | ---- |"                                                                >> readme.md
-echo "|${fig_number_scalar_order_parameter_passive_homeotropic}| ---- | ---- |"              >> readme.md
+for ((i=0; i<${#fig_number_in_paper[@]}; i++))
+do
+	echo "|${fig_number_in_paper[${i}]}|||" >> readmd.md
+done
