78f22cc3281ded475acae222fde7afac50fd8042
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-19 21:34:33 +0900

    update readme etc.

--- copy_and_rename_dat_files.sh
+++ copy_and_rename_dat_files.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 # copy_and_rename_dat_files.sh
 
-###   ###
-###   ### STEP 1: Get list of figure number in the paper.
-###   ###
-###   #source ./list_figure_numbers.txt
-###   fig_number_scalar_order_parameter_passive_homeotropic="3a"
-###   ###
-###   ### STEP 2: Copy and rename dat files.
-###   ###
-###   path_to_datafile_scalar_order_parameter_passive_homeotropic="/Volumes/matsukiyo.hiroki/Documents/simulation11_Angheluta_with_viscosity_and_with_pillar/RESULTS_REV/N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis/scalar_order_parameter_S_N_480_M_1000_Delta_t_1.0e-1_Q_init_0.7071_R_0_1.0e2_bc_in_homeotropic_bc_out_parallel_to_x_axis_step_0000200000.dat"
-###   if [ ! -e ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ]; then
-###   	echo -e "\033[0;31m
-###   	Can't find datafile.
-###   	filename:
-###   	${path_to_datafile_scalar_order_parameter_passive_homeotropic}
-###   	\033[0;39m"
-###   	exit 1
-###   fi
-###   mkdir -p data_files
-###   cp ${path_to_datafile_scalar_order_parameter_passive_homeotropic} ./data_files/
-###
-###
-###
 ### Source configuration files
-sourced_file="../code/list_short_name_of_fig.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_fig_number_in_paper.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_fig_number_in_paper_converted_to_integer.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_skipped_fig_in_correspondence_table.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_figures_made_of_single_or_multiple_data_file.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_gnuplot_script_name.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-sourced_file="./list_short_data_file_name.txt"
-if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
-#source ./list_data_files.txt
+sourced_files=()
+sourced_files+=("../code/list_short_name_of_fig.txt")
+sourced_files+=("./list_fig_number_in_paper.txt")
+sourced_files+=("./list_fig_number_in_paper_converted_to_integer.txt")
+sourced_files+=("./list_skipped_fig_in_correspondence_table.txt")
+sourced_files+=("./list_figures_made_of_single_or_multiple_data_file.txt")
+sourced_files+=("./list_gnuplot_script_name.txt")
+sourced_files+=("./list_short_data_file_name.txt")
+sourced_files+=("./list_full_data_file_name.txt")
+for sourced_file in ${sourced_files[@]}
+do
+	if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
+done
+### Clean readme.md and data_files/
 : > readme.md
+rm -f ./data_files/*
 echo "# Correspondence table between figure names, data files and scripts to create figures" >> readme.md
-echo "|Figure Number|Data file (in \"data_files\" directory)|Script (in \"scripts\" directory)|" >> readme.md
-echo "| ---- | ---- | ---- |" >> readme.md
+echo "|Figure Number|Data file (in \"data_files\" directory)|Script (in \"scripts\" directory)|How to execute gp script|" >> readme.md
+echo "| ---- | ---- | ---- | ---- |" >> readme.md
 for ((i=0; i<${#fig_number_in_paper_converted_to_integer[@]}; i++))
 do
 	for ((j=0; j<${#fig_number_in_paper_converted_to_integer[@]}; j++))
@@ -52,19 +30,27 @@ do
 				if [ -z "${skipped_fig_in_correspondence_table[${j}]}" ]; then
 					if [ "${made_of_single_or_multiple_data_file[${j}]}" = "no" ]; then
 						### Copy data file and rename
-						# chech file existence
+						# check file existence
 						### Output Fig Number, data file name and script name
-						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}||" >> readme.md
 					elif [ "${made_of_single_or_multiple_data_file[${j}]}" = "single" ]; then
 						### Copy data file and rename
-						# chech file existence
+						# check file existence & copy & rename
+						if [ -n "${full_data_file_name[${j}]}" ]; then
+							if [ -e ${full_data_file_name[${j}]} ]; then
+								cp ${full_data_file_name[${j}]} ./data_files/${short_data_file_name[${j}]}.dat
+							else
+								echo "Can't find ${full_data_file_name[${j}]}"
+								exit 1
+							fi
+						fi
 						### Output Fig Number, data file name and script name
-						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}||" >> readme.md
 					elif [ "${made_of_single_or_multiple_data_file[${j}]}" = "multiple" ]; then
 						### Copy data file and rename
-						# chech file existence
+						# check file existence
 						### Output Fig Number, data file name and script name
-						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${fig_number_in_paper[${j}]}|${short_data_file_name[${j}]}|${gnuplot_script_name[${j}]}||" >> readme.md
 					fi
 				fi
 			fi
--- readme.md
+++ readme.md
@@ -1,23 +1,23 @@
 # Correspondence table between figure names, data files and scripts to create figures
-|Figure Number|Data file (in "data_files" directory)|Script (in "scripts" directory)|
-| ---- | ---- | ---- |
-|3a|short_data_file_name|scalar_order_parameter.gp|
-|3b|short_data_file_name|scalar_order_parameter.gp|
-|3c|short_data_file_name|director.gp|
-|3d|short_data_file_name|director.gp|
-|4a|short_data_file_name|scalar_order_parameter.gp|
-|4b|short_data_file_name|director.gp|
-|4c|short_data_file_name||
-|4d|short_data_file_name|scalar_order_parameter.gp|
-|4e|short_data_file_name|director.gp|
-|4f|short_data_file_name||
-|5a|short_data_file_name|scalar_order_parameter.gp|
-|5b|short_data_file_name|director.gp|
-|5c|short_data_file_name||
-|5d|short_data_file_name|scalar_order_parameter.gp|
-|5e|short_data_file_name|director.gp|
-|5f|short_data_file_name||
-|6a|short_data_file_name||
-|6b|short_data_file_name||
-|7a|short_data_file_name||
-|7b|short_data_file_name||
+|Figure Number|Data file (in "data_files" directory)|Script (in "scripts" directory)|How to execute gp script|
+| ---- | ---- | ---- | ---- |
+|3a|short_data_file_name|scalar_order_parameter.gp||
+|3b|short_data_file_name|scalar_order_parameter.gp||
+|3c|short_data_file_name|director.gp||
+|3d|short_data_file_name|director.gp||
+|4a|short_data_file_name|scalar_order_parameter.gp||
+|4b|short_data_file_name|director.gp||
+|4c|short_data_file_name|||
+|4d|short_data_file_name|scalar_order_parameter.gp||
+|4e|short_data_file_name|director.gp||
+|4f|short_data_file_name|||
+|5a|short_data_file_name|scalar_order_parameter.gp||
+|5b|short_data_file_name|director.gp||
+|5c|short_data_file_name|||
+|5d|short_data_file_name|scalar_order_parameter.gp||
+|5e|short_data_file_name|director.gp||
+|5f|short_data_file_name|||
+|6a|short_data_file_name|||
+|6b|short_data_file_name|||
+|7a|short_data_file_name|||
+|7b|short_data_file_name|||
