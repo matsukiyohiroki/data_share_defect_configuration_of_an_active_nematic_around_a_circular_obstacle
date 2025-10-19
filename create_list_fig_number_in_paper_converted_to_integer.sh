4cca593d588a1a6635dff5058bd4773849b8407d
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-19 17:20:36 +0900

    update create_list_fig_number_in_paper_converted_to_integer and list_fig_number_in_paper_converted_to_integer

--- create_list_fig_number_in_paper_converted_to_integer.sh
+++ create_list_fig_number_in_paper_converted_to_integer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#数字＋アルファベットのFIG番号を取得
 source list_fig_number_in_paper.txt
 
 # 空文字以外のインデックスと値を取得
@@ -11,9 +12,6 @@ for i in "${!fig_number_in_paper[@]}"; do
 		values+=("${fig_number_in_paper[${i}]}")
 	fi
 done
-echo "${fig_number_in_paper[@]}"
-echo "${indices[@]}"
-echo "${values[@]}"
 
 # 非空要素だけをソート（macOS向け）
 sorted_values=($(
@@ -22,25 +20,19 @@ sorted_values=($(
 		| sort -k1,1n -k2,2 \
 		| tr -d ' '
 			))
-echo "${fig_number_in_paper[@]}"
-echo "${indices[@]}"
-echo "${values[@]}"
-echo "${sorted_values[@]}"
 
 # 結果表示
 : > list_fig_number_in_paper_converted_to_integer.txt
 for ((i=0; i<${#fig_number_in_paper[@]}; i++));
 do
-	#printf 'array[%d]="%s"\n' "$i" "${array[i]}"
 	if [ -n "${fig_number_in_paper[${i}]}" ]; then
 		for ((k=0; k<${#sorted_values[@]}; k++));
 		do
-			#echo "${sorted_values[${k}]}"
 			if [ "${fig_number_in_paper[${i}]}" = "${sorted_values[${k}]}" ]; then
-				printf 'fig_number_in_paper[%d]="%d"\n' "${i}" "${k}"
+				printf 'fig_number_in_paper_converted_to_integer[%d]="%d"\n' "${i}" "${k}" >> list_fig_number_in_paper_converted_to_integer.txt
 			fi
 		done
 	else
-		printf 'fig_number_in_paper[%d]=""\n' "$i"
+		printf 'fig_number_in_paper_converted_to_integer[%d]=""\n' "$i"                            >> list_fig_number_in_paper_converted_to_integer.txt
 	fi
 done
