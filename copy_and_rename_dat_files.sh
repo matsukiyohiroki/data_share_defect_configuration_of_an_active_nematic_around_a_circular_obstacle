6516cf6ee198f56a3c201df477d5284afa1e2a4f
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-22 15:44:22 +0900

    update copy_and_rename_dat_files.sh.

--- copy_and_rename_dat_files.sh
+++ copy_and_rename_dat_files.sh
@@ -16,17 +16,18 @@ do
 	if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
 done
 ### Clean readme.md and data_files/
-: > readme.md
+README_MD_FILE="readme.md"
+: > ${README_MD_FILE}
 rm -f ./data_files/*
 # title of readme
-echo "# Correspondence table between figure names, data files and scripts to create figures" >> readme.md
+echo "# Correspondence table between figure names, data files and scripts to create figures" >> ${README_MD_FILE}
 # Notes on how to use scripts
-echo "Notes" >> readme.md
-echo -e "- Please execute scripts as \`\`./script.sh output.pdf\`\`" >> readme.md
-echo -e "- The figures in the paper were created on Linux (Ubuntu).\n" >> readme.md
+echo "Notes" >> ${README_MD_FILE}
+echo -e "- Please execute scripts as \`\`./script.sh output.pdf\`\`" >> ${README_MD_FILE}
+echo -e "- The figures in the paper were created on Linux (Ubuntu).\n" >> ${README_MD_FILE}
 # Correspondence table
-echo "|Figure Number|Data file (in \"data_files\" directory)|Script (in \"scripts\" directory)|" >> readme.md
-echo "| ---- | ---- | ---- |" >> readme.md
+echo "|Figure Number|Data file (in \"data_files\" directory)|Script (in \"scripts\" directory)|" >> ${README_MD_FILE}
+echo "| ---- | ---- | ---- |" >> ${README_MD_FILE}
 for ((i=0; i<${#fig_number_in_paper_converted_to_integer[@]}; i++))
 do
 	for ((j=0; j<${#fig_number_in_paper_converted_to_integer[@]}; j++))
@@ -38,7 +39,7 @@ do
 						### Copy data file and rename
 						# check file existence
 						### Output Fig Number, data file name and script name
-						echo "|${fig_number_in_paper[${j}]}|-|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${fig_number_in_paper[${j}]}|-|${gnuplot_script_name[${j}]}|" >> ${README_MD_FILE}
 					elif [[ "${short_data_file_name[${j}]}" != *$'\n'* ]]; then
 						### Copy data file and rename
 						###   # check file existence
@@ -50,26 +51,25 @@ do
 						###   	exit 1
 						###   fi
 						### Output Fig Number, data file name and script name
-						echo "|${fig_number_in_paper[${j}]}|[${short_data_file_name[${j}]}](./data_files/${short_data_file_name[${j}]})|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${fig_number_in_paper[${j}]}|[${short_data_file_name[${j}]}](./data_files/${short_data_file_name[${j}]})|${gnuplot_script_name[${j}]}|" >> ${README_MD_FILE}
 					elif [[ "${short_data_file_name[${j}]}" == *$'\n'* ]]; then
 						IFS=$'\n' read -r -d '' -a multiple_dat_files <<< "${short_data_file_name[${j}]}"
-						echo -n "|${fig_number_in_paper[${j}]}|" >> readme.md
+						echo -n "|${fig_number_in_paper[${j}]}|" >> ${README_MD_FILE}
 						for ((k=0; k<${#multiple_dat_files[@]}; k++))
 						do
 							# check file existence
 							# copy & rename
 							### Output Fig Number, data file name and script name
 							if (( k == ${#multiple_dat_files[@]} -1 )); then
-								echo -n "[${multiple_dat_files[${k}]}](./data_files/${multiple_dat_files[${k}]})" >> readme.md
+								echo -n "[${multiple_dat_files[${k}]}](./data_files/${multiple_dat_files[${k}]})" >> ${README_MD_FILE}
 							else
-								echo -n "[${multiple_dat_files[${k}]}](./data_files/${multiple_dat_files[${k}]})<br>" >> readme.md
+								echo -n "[${multiple_dat_files[${k}]}](./data_files/${multiple_dat_files[${k}]})<br>" >> ${README_MD_FILE}
 							fi
 						done
-						echo "|${gnuplot_script_name[${j}]}|" >> readme.md
+						echo "|${gnuplot_script_name[${j}]}|" >> ${README_MD_FILE}
 					fi
 				fi
 			fi
 		fi
 	done
 done
-echo "" >> readme.md
