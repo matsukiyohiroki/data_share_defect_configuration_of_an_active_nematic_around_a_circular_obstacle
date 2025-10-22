9c7413e0a0734e336e45615ab1d9ecee919e560b
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-23 02:40:08 +0900

    update gp scripts.

--- scripts/execute_gp_script.sh
+++ scripts/execute_gp_script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ### Error handling for argument.
 if [ $# != 1 ]; then
-	echo -e "\nThe number of arguments is not correct!\nTerminate the script.\nThe number of argument of this script must be 1.\n"
+	echo "
+	The number of arguments is not correct!
+	Terminate the script.
+	The number of argument of this script must be 1.
+	"
 	exit 1
 fi
 if [ ! -e "./$1" ]; then
@@ -12,13 +16,37 @@ fi
 mkdir -p ../output_pdf_files
 ### Execute gnuplot script
 if [ "$1" = "velocity_on_y_axis_homeotropic_positive_alpha.gp" ]; then
-	echo "$1"
+	simulation_r_d_dat_file="../data_files/time_evolution_of_r_d_homeotropic_positive_alpha.dat"
+	lastline=`tail -1 ${simulation_r_d_dat_file}`
+	r_d=`echo ${lastline} | cut -f 2 -d " "`
+	gnuplot -e "
+	r_d=${r_d};
+	load '$1';
+	"
 elif [ "$1" = "velocity_on_y_axis_homeotropic_negative_alpha.gp" ]; then
-	echo "$1"
+	simulation_r_d_dat_file="../data_files/time_evolution_of_r_d_homeotropic_negative_alpha.dat"
+	lastline=`tail -1 ${simulation_r_d_dat_file}`
+	r_d=`echo ${lastline} | cut -f 2 -d " "`
+	gnuplot -e "
+	r_d=${r_d};
+	load '$1';
+	"
 elif [ "$1" = "velocity_on_y_axis_homogeneous_positive_alpha.gp" ]; then
-	echo "$1"
+	simulation_r_d_dat_file="../data_files/time_evolution_of_r_d_homogeneous_positive_alpha.dat"
+	lastline=`tail -1 ${simulation_r_d_dat_file}`
+	r_d=`echo ${lastline} | cut -f 2 -d " "`
+	gnuplot -e "
+	r_d=${r_d};
+	load '$1';
+	"
 elif [ "$1" = "velocity_on_y_axis_homogeneous_negative_alpha.gp" ]; then
-	echo "$1"
+	simulation_r_d_dat_file="../data_files/time_evolution_of_r_d_homogeneous_negative_alpha.dat"
+	lastline=`tail -1 ${simulation_r_d_dat_file}`
+	r_d=`echo ${lastline} | cut -f 2 -d " "`
+	gnuplot -e "
+	r_d=${r_d};
+	load '$1';
+	"
 else
 	gnuplot -e "load '$1'"
 fi
---
+++ scripts/velocity_on_y_axis_homogeneous_positive_alpha.gp
@@ -0,0 +1,28 @@
+#################################### Settings for terminal and output.
+set terminal pdfcairo enhanced size 5in, 5in;
+set output "../output_pdf_files/velocity_on_y_axis_homogeneous_positive_alpha.pdf";
+#################################### Parameters for visualization
+xrange_r=+140.0;
+xrange_l=+100.0;
+yrange_t=+0.003;
+yrange_b=-0.010;
+#################################### Settings on margin, x,yrange, title, x,ylabel, etc...
+set tmargin at screen 0.92;
+set bmargin at screen 0.30;
+set xrange [xrange_l:xrange_r];
+set yrange [yrange_b:yrange_t];
+set key top left font "Arial,18";
+set xlabel "y" font "Arial,18";
+set ylabel "u_{r}" font "Arial,18";
+set tics font "Arial,16";
+#################################### Plot
+datfile = "../data_files/velocity_on_y_axis_homogeneous_positive_alpha.dat";
+set xzeroaxis lt -1 lw 2;
+set parametric;
+set trange [yrange_b:yrange_t];
+plot datfile using 1:2 lc "black" pt 2 lw 3 notitle, \
+	     r_d,t with lines lw 3 lc "red" title "Defect Position";
+#################################### Unset settings.
+unset parametric;
+unset output;
+unset terminal;
