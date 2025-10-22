d6fc18567bdde4d6a78e136ff2a358a612fd7f3f
matsukiyohiroki_macbookair <h.matsukiyo@cmt.phys.kyushu-u.ac.jp>
2025-10-23 00:28:13 +0900

    update copy_and_rename_dat_files.sh and readme.md.

--- copy_and_rename_dat_files.sh
+++ copy_and_rename_dat_files.sh
@@ -12,6 +12,7 @@ sourced_files+=("./list_gnuplot_script_name.txt")
 sourced_files+=("./list_short_data_file_name.txt")
 sourced_files+=("./list_full_data_file_name.txt")
 sourced_files+=("./list_output_pdf_files_name.txt")
+sourced_files+=("./list_shell_script_name.txt")
 for sourced_file in ${sourced_files[@]}
 do
 	if [ -e ${sourced_file} ]; then source ${sourced_file}; else echo "Can't find ${sourced_file}. Terminate the script."; exit 1;fi
